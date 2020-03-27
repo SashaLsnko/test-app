45e149ef5e36966a98419c39ba7fa66e5c2654c8
Mark Li <markl6943@gmail.com>
2020-03-27 13:13:12 -0400

    add test

--- src/App.js
+++ src/App.js
@@ -2,22 +2,35 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const handleOpen = ()=>{
+  
+  const now = new Date().valueOf()
+  const url = window.location.href;
+  setTimeout(()=>{
+    if(new Date().valueOf()-now > 100) return;
+    window.location.assign(
+      "https://apps.apple.com/ca/app/walmart-shopping-groceries/id1084793516"
+    );
+    // setTimeout(()=>{
+    //   window.location.assign(
+    //     url
+    //   );
+    // })
+  },25)
+  window.location.assign(`walmartapp://section/${url}`)
+}
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {navigator.userAgent}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {(navigator.userAgent.indexOf("CriOS") !== -1) &&
+         <button onClick={handleOpen}> Open</button>}
+
       </header>
     </div>
   );
