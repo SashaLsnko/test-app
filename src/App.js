95be2b723389f18b9063c860f834f53c47b140c3
Mark Li <markl6943@gmail.com>
2020-03-30 10:37:45 -0400

    fix regex

--- src/App.js
+++ src/App.js
@@ -10,17 +10,17 @@ const handleOpen = ()=>{
     if(new Date().valueOf()-now > 100) return;
     window.location.assign(
       "https://apps.apple.com/ca/app/walmart-shopping-groceries/id1084793516"
-    );
-    // setTimeout(()=>{
-    //   window.location.assign(
-    //     url
-    //   );
-    // })
-  },25)
-  window.location.assign(`walmartapp://section/${url}`)
-}
-
-function App() {
+      );
+      // setTimeout(()=>{
+        //   window.location.assign(
+          //     url
+          //   );
+          // })
+        },25)
+        window.location.assign(`walmartapp://section/${url}`)
+      }
+      
+      function App() {
   return (
     <div className="App">
       <header className="App-header">
@@ -28,7 +28,7 @@ function App() {
         <p>
           {navigator.userAgent}
         </p>
-        {(navigator.userAgent.indexOf("CriOS") !== -1) &&
+        {(/CriOS|Android.*Chrome/.test(navigator.userAgent)) &&
          <button onClick={handleOpen}> Open</button>}
 
       </header>
